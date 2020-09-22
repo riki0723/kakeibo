e41d3d576a56abb4ef07e09f95208dc58d713399
riki0723 <rikishibata0723@gmail.com>
2020-09-22 21:50:38 +0900

    バリデーションの設定

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,9 @@
 class Expense < ApplicationRecord
+  belongs_to :user
+
+
+  validates :name, :price, :wheretobuy, :category_id, :payment_id, presence: true
+  validates :category_id, :payment_id, numericality: { other_than: 1 } 
+
+
 end
