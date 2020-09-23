56a6e51ec74708627a8dbbea70f2c522ac271838
riki0723 <rikishibata0723@gmail.com>
2020-09-23 21:08:03 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "expenses#index"
-    resources :expenses, only: [:index, :new, :create, :show, :destroy]
+    resources :expenses, only: [:index, :new, :create, :show, :destroy, :edit, :update]
 end
