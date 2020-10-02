3734726534f9269741cee6ca1d355006d785809f
riki0723 <rikishibata0723@gmail.com>
2020-10-02 14:11:00 +0900

    topページの表示

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,4 +1,9 @@
-window.addEventListener('load', function(){
+function main() {
+  const show_top = document.getElementById("show-top");
+  const pull_down_top = document.getElementById("pull-down-top");
+  const show_top_top = document.getElementById("show-top-top");
+
+
   const show_calendar = document.getElementById("show-calendar");
   const pull_down_calendar = document.getElementById("pull-down-calendar");
   const show_top_calender = document.getElementById("show-top-calender");
@@ -9,15 +14,30 @@ window.addEventListener('load', function(){
   const show_top_list = document.getElementById("show-top-list");
 
 
+
+  show_top.addEventListener('click', function() {
+    // 表示するもの
+    pull_down_top.setAttribute("style", "display:block;")
+    show_top_top.setAttribute("style", "display:block;")
+    // 表示しないもの
+    pull_down_rireki.removeAttribute("style", "display:block;")
+    show_top_list.removeAttribute("style", "display:block;")
+    pull_down_calendar.removeAttribute("style", "display:block;")
+    show_top_calender.removeAttribute("style", "display:block;")
+
+  })
+
+
   
   show_calendar.addEventListener('click', function() {
-    // console.log("click OK")
     // 表示するもの
     pull_down_calendar.setAttribute("style", "display:block;")
     show_top_calender.setAttribute("style", "display:block;")
     // 表示しないもの
     pull_down_rireki.removeAttribute("style", "display:block;")
     show_top_list.removeAttribute("style", "display:block;")
+    pull_down_top.setAttribute("style", "display: none;")
+    show_top_top.setAttribute("style", "display: none;")
 
   })
 
@@ -26,8 +46,13 @@ window.addEventListener('load', function(){
     // 表示するもの
     pull_down_rireki.setAttribute("style", "display:block;")
     show_top_list.setAttribute("style", "display:block;")
-    // 隠すもの
+    // 表示しないもの
     pull_down_calendar.removeAttribute("style", "display:block;")
     show_top_calender.removeAttribute("style", "display:block;")
+    pull_down_top.setAttribute("style", "display: none;")
+    show_top_top.setAttribute("style", "display: none;")
+
   })
-})
+}
+
+window.addEventListener("load", main);
