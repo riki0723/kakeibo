4a617a13beb03b8e459cf12afcfbbf00c6b3186d
riki0723 <rikishibata0723@gmail.com>
2020-10-07 00:16:06 +0900

    検索機能と表示機能実装

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,8 +1,11 @@
 class ExpensesController < ApplicationController
 
   def index
-    @expenses = Expense.all
-    @expenses = Expense.page(params[:page]).per(10)
+    @expenses = Expense.all.order("created_at DESC")
+
+
+    @q = Expense.ransack(params[:q])
+    @expenses = @q.result(distinct: true).page(params[:page]).per(10)
   end
 
 
