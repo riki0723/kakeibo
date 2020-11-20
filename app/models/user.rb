a9ee9d77b6467705e0a89beec3c98a4add40875c
riki0723 <rikishibata0723@gmail.com>
2020-11-21 00:29:30 +0900

    userモデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
          has_many :plans
 
 
-  validates :nickname, :name,presence: true
+  validates :nickname, :name, :password,presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
